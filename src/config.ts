9ff9e16aa101c92a903772f401656be4ff4336cb
Simen A. W. Olsen <so@bjerk.io>
2022-11-28 10:40:54 +0100

    Add SELF_URL to config

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,7 @@ import { cleanEnv, num, str } from 'envalid';
 
 export const config = cleanEnv(process.env, {
   PORT: num({ default: 8080 }),
+  SELF_URL: str({ default: 'http://localhost:8080' }),
   LOG_LEVEL: str({
     default: 'info',
     choices: ['trace', 'debug', 'info', 'warn', 'error', 'fatal'],
