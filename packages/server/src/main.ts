39c31680438df64ebe69bdbab9750801a5636ba1
Simen A. W. Olsen <so@bjerk.io>
2023-07-29 20:32:11 +0200

    refactor: renaming packages

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,12 +1,11 @@
 import { config } from './config.js';
-import { makeAdminServer, makePublicServer } from './server.js';
+import { makeManagementServer, makePublicServer } from './server.js';
 
-const publicServer = await makePublicServer();
-const adminServer = await makeAdminServer();
-
-const listen = Promise.all([
-  publicServer.listen({ host: '0.0.0.0', port: config.PORT }),
-  adminServer.listen({ host: '0.0.0.0', port: config.ADMIN_PORT }),
+await Promise.all([
+  makePublicServer().then(server =>
+    server.listen({ host: '0.0.0.0', port: config.PORT }),
+  ),
+  makeManagementServer().then(server =>
+    server.listen({ host: '0.0.0.0', port: config.ADMIN_PORT }),
+  ),
 ]);
-
-void (await listen);
